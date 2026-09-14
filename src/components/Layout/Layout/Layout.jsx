7dbdf847c070f96ceb0ce8987d6fc0158726f2b9
janninobansag <janninobansag@gmail.com>
2026-09-14 14:46:46 +0800

    updated UI

--- src/components/Layout/Layout/Layout.jsx
+++ src/components/Layout/Layout/Layout.jsx
@@ -2,10 +2,10 @@ import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import './Layout.css'
 
-const Layout = ({ children, isLoggedIn, onLogout, onNavigate }) => {
+const Layout = ({ children, isLoggedIn, onLogout, onNavigate, theme, onToggleTheme }) => {
   return (
     <div className="layout">
-      <Header isLoggedIn={isLoggedIn} onLogout={onLogout} onNavigate={onNavigate} />
+      <Header isLoggedIn={isLoggedIn} onLogout={onLogout} onNavigate={onNavigate} theme={theme} onToggleTheme={onToggleTheme} />
       <main className="layout-main">
         {children}
       </main>
